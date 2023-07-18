a6b8743da7e4f98d91caf23278468a488e3a78e4
TomyLM02 <tmelazzo@wpi.edu>
2023-07-18 16:27:14 -0400

    hkijhtijthjtigtjhi

--- robpong/Ball.js
+++ robpong/Ball.js
@@ -6,11 +6,53 @@ class Ball {
         this.vy = vy;
         this.r = r;
     }
-
+   
     draw() {
-        ctx.fillStyle = "yellow";
+        ctx.fillStyle = "white";
         ctx.strokeStyle = "black";
         ctx.lineWidth = 2;
-        ctx.beginPath()
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+    bounceWall() {
+        // TOP WALL
+        if (this.y < this.r) {
+            this.vy = 1 * Math.abs(this.vy);
+        }
+        // BOTTOM WALL
+        if (this.y > BOARDHEIGHT - this.r) {
+            this.vy = -1 * Math.abs(this.vy);
+        }
+    }
+    bouncePaddleL (paddle) {
+        if (this.x - this.r > paddle.w) return false;
+        if (this.x - this.r < 0) return true;
+        if (this.y < paddle.y) return false;
+        if (this.y > paddle.y + paddle.l) return false;
+        if (this.vx < 0) {
+            this.vx = PADDLEFORCE * Math.abs(this.vx);
+            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos * PADDLESPIN;
+        }
+        return false;
+    }
+    bouncePaddleR (paddle) {
+        if (this.x + this.r < paddle.x) return false;
+        if (this.x + this.r > paddle.x + paddle.w) return true;
+        if (this.y < paddle.y) return false;
+        if (this.y> paddle.y + paddle.l) return false;
+        if (this.vx > 0) {
+            this.vx = -PADDLEFORCE * Math.abs(this.vx);
+            let paddlePos = (this.y - paddle.y - paddle.l/2) / paddle.l * 2; // between -1.0 and 1.0
+            this.vy = this.vy + paddlePos * PADDLESPIN;
+        }
+        return false;
+    }
+   
+    move() {
+        this.x +=this.vx;
+        this.y +=this.vy;
     }
 }
